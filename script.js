b63fffff9df0428294b5305d22bf4f4b49f85a25
Ian Cappellani <ian.cappellani@mail.utoronto.ca>
2020-06-14 14:34:39 -0400

    Clicking outside mobile menu closes it, and made iframe not display on small screens.

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
+function hideOnClickOutside(element) {
+    const outsideClickListener = event => {
+        if (!element.contains(event.target) 
+            && !document.querySelector("#mobile_menu_button").contains(event.target)
+            && !element.classList.contains("hidden")) {
+          element.classList.add("hidden")
+          removeClickListener()
+        }
+    }
+
+    const removeClickListener = () => {
+        document.removeEventListener('click', outsideClickListener)
+    }
+
+    document.addEventListener('click', outsideClickListener)
+}
+
 function toggleMobileMenu () {
+    console.log("toggle")
     const menu = document.querySelector("#main_nav ul")
-    menu.classList.toggle("hidden")
+    // menu.classList.toggle("hidden")
+    if (menu.classList.contains("hidden")) {
+        console.log("starts hidden")
+        menu.classList.remove("hidden")
+        hideOnClickOutside(menu)
+    } else {
+        menu.classList.add("hidden")
+    }
 }
 
 document.querySelector("#mobile_menu_button").addEventListener("click", toggleMobileMenu)
